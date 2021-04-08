b3b9c53eb7699c7020cd551a5f890a79c5f28897
miller78130 <miller78130@gmail.com>
2021-04-07 22:23:04 -0500

    4-7-2021

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,14 +1,30 @@
 import os
 import csv
-print(os.getcwd())
+
+# Define Pathway
 budget_data = os.path.join('Resources','budget_data.csv')
+
 # Total number of Months
 with open(budget_data, "r") as csv_file:
     reader = csv.reader(csv_file, delimiter=',')
-    total_months = len(list(reader))
     
+    months = len(list(reader))
+    total_months = months - 1
+
+with open(budget_data, "r") as csv_file:
+    calc = csv.reader(csv_file, delimiter=',')
+    total = 0
+    for row in csv.reader(calc):
+        print (col)
+        for col in row[1]:
+            total += int(row[1])
+
+
     
+    print(f"Financial Analysis")
+    print (f"-------------------------------------------------------------")
     print(f"Total Months: {total_months}")
+    print(f"Total: $")
 
 #net total of profit/losses over the entire period
 #with open(budget_data, "r") as csv_file:
